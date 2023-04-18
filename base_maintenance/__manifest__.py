1bb29031d7080426eb192b188b7eaca88de04b40
GIG-ERP <93262473+GIG-ERP@users.noreply.github.com>
2023-04-18 21:03:23 +0300

    First Upload

---
+++ base_maintenance/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023
+# License LGPL-3.0 or later ().
+
+{
+    "name": "Base Maintenance",
+    "version": "15.0.1.1.0",
+    "author": "",
+    "development_status": "Beta",
+    "website": "",
+    "category": "Maintenance",
+    "license": "LGPL-3",
+    "depends": ["maintenance"],
+    "data": [
+        "views/maintenance_team_views.xml",
+        "views/maintenance_request_views.xml",
+        "views/maintenance_equipment_views.xml",
+        "views/report_maintenance_request.xml",
+    ],
+    "installable": True,
+}
---
+++ maintenance_plan/README.rst
@@ -0,0 +1,7 @@
+================
+Maintenance Plan
+================
+
+
+This module extends the functionality of Odoo Maintenance module by allowing
+an equipment to have different preventive maintenance kinds.
