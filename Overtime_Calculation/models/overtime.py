9428b93b248c1ec20a24bc9495807e8687c181fa
GIG-ERP <93262473+GIG-ERP@users.noreply.github.com>
2023-10-30 11:13:32 +0300

    Second commit
    calculate with 208 hr

--- Overtime_Calculation/models/overtime.py
+++ Overtime_Calculation/models/overtime.py
@@ -13,10 +13,10 @@ class HrContract(models.Model):
     @api.depends('worked_hour1', 'worked_hour2', 'worked_hour3','worked_hour4', 'wage')
     def _compute_total_overtime(self):
         for record in self:
-            total_overtime1 = record.wage / 240 * (record.worked_hour1 * 1.5)
-            total_overtime2 = record.wage / 240 * (record.worked_hour2 * 1.75)
-            total_overtime3 = record.wage / 240 * (record.worked_hour3 * 2)
-            total_overtime4 = record.wage / 240 * (record.worked_hour4 * 2.5)
+            total_overtime1 = record.wage / 208 * (record.worked_hour1 * 1.5)
+            total_overtime2 = record.wage / 208 * (record.worked_hour2 * 1.75)
+            total_overtime3 = record.wage / 208 * (record.worked_hour3 * 2)
+            total_overtime4 = record.wage / 208 * (record.worked_hour4 * 2.5)
 
             record.total_overtime = total_overtime1 + total_overtime2 + total_overtime3 + total_overtime4
             record.x_add_overtime = record.total_overtime
